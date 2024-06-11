f1f1251eecc344e435df235808ac03cd5bd63d72
ben <bcmulford@gmail.com>
2024-06-11 16:28:32 -0500

    add toast component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import { getToastStore, initializeStores, Toast } from '@skeletonlabs/skeleton';
+    // import type { ToastSettings, ToastStore } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
-</script>
 
+    initializeStores();
+    const toastStore = getToastStore();
+</script>
+<Toast />
 <slot></slot>
 
 <style></style>
